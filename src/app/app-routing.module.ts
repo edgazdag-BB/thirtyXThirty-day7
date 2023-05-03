aa886a1960df89b7e2e69415b5f853be387a60ff
Ed Gazdag <ed.gazdag@briebug.com>
2023-05-03 09:27:43 -0400

    Updates from PR comments.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,11 +26,7 @@ const routes: Routes = [
         path: '',   
         redirectTo: 'home', 
         pathMatch: 'full' 
-      },
-      { 
-        path: '**', 
-        component: PageNotFoundComponent 
-      },
+      }
     ]
   },
   { path: '**', component: PageNotFoundComponent }
